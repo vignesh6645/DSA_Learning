4cdeb4bcceb713731f3ca026ea1489ab6a7596e9
vignesh6645 <86340986+vignesh6645@users.noreply.github.com>
2026-08-12 18:57:22 +0530

    Add: products-of-array-discluding-self - submission-0

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-0.go
@@ -0,0 +1,21 @@
+func productExceptSelf(nums []int) []int {
+	n := len(nums)
+	if n == 0 {
+		return []int{}
+	}
+
+	result := make([]int, n)
+	result[0] = 1
+
+	for i := 1; i < n; i++ {
+		result[i] = result[i-1] * nums[i-1]
+	}
+
+	rightProd := 1
+	for i := n - 1; i >= 0; i-- {
+		result[i] *= rightProd
+		rightProd *= nums[i]
+	}
+
+	return result
+}
